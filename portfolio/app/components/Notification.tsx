200a03e878326e61105e87ee0f69e5714cc19271
Mitchell Conrad <Mitchell.conrad@gmail.com>
2023-11-10 19:46:36 -0500

    Fixed Notifications for Light Mode

--- portfolio/app/components/Notification.tsx
+++ portfolio/app/components/Notification.tsx
@@ -5,23 +5,15 @@ const Notification = (props: { title: string, open: boolean, handleClose: () => 
     if(!props.open) 
         return(<></>)
 
-    const handleCloseHandler = (e: React.MouseEvent<HTMLElement>) => {
-        let t = e.target as HTMLInputElement
-        if(t.id == "wrapper"){
-            props.handleClose()
-        } 
-    }
-
     let num = props.percent + '%'
 
     return(
         <div id='wrapper'
-            className='fixed top-0 left-0 right-0 flex justify-center'
-            onClick={handleCloseHandler}>
+            className='fixed top-0 left-0 right-0 flex justify-center'>
 
             {/* Notification Content */}
             <div id='content'
-                className='w-72 flex flex-col bg-white dark:bg-lightGray mt-10 p-5 rounded-lg'>
+                className='w-72 flex flex-col shadow-2xl bg-white dark:bg-lightGray mt-10 p-5 rounded-lg'>
                 <div className='flex justify-between'>
                     <p className='text-sm font-semibold'>{ props.title }</p>
                     <AiOutlineClose className='mt-1 text-xl cursor-pointer' onClick={ () => props.handleClose() } />
@@ -31,9 +23,8 @@ const Notification = (props: { title: string, open: boolean, handleClose: () => 
                 { props.children }
 
                 {/* Progress Bard */}
-                <div className='w-full h-2 mt-2 rounded-md dark:bg-gray'>
-                    <div className='h-2 rounded-md dark:bg-analagous1' style={{width: num}}/>
-                    {/* className={`'w-[${num}%] h-2 rounded-md dark:bg-analagous1'`}  */}
+                <div className='w-full h-2 mt-2 rounded-md bg-gray'>
+                    <div className='h-2 rounded-md bg-gradient-to-r from-analagous1 to-primary' style={{width: num}}/>
                 </div>
             </div>              
         </div>
