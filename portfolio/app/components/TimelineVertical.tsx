9442e6a0543cb6e5350d124e58a1969abde5972b
Mitchell Conrad <Mitchell.conrad@gmail.com>
2024-01-02 23:19:05 -0500

    Update TimelineVertical.tsx
    Vertical timeline complete with grid

--- portfolio/app/components/TimelineVertical.tsx
+++ portfolio/app/components/TimelineVertical.tsx
@@ -2,69 +2,115 @@ import React from 'react'
 
 const TimelineVertical = () => {
   return (
-    <>
-        
+    <div className='grid grid-cols-7 gap-x-2'>
+        {/* Row 1 */}
+        <div className='col-span-3'/>
         <div className='flex'>
-            <div className='px-1 h-full mt-24'>
-                <div className='flex'>
-                    <div className='px-2 pb-2'>
-                        <p className='font-bold'>Becton Dickinson</p>
-                        <p className='text-sm font-semibold'>May-Aug 2022</p>
-                        <p className='text-sm'>Software Engineering Intern</p>
-                    </div>
-                    <div className='w-20 h-0.5 mt-3 rounded-md bg-gray dark:bg-white' />
-                    <div className='w-2 h-20 rounded-md bg-triadic2 ml-auto' />
-                </div>
-                <div className='w-2 h-20 rounded-md' />
-                <div className='flex'>
-                    <div className='px-2 pb-2'>
-                        <p className='font-bold'>MDI</p>
-                        <p className='text-sm font-semibold'>Jan-May 2023</p>
-                        <p className='text-sm'>Controls Engineering Intern</p>
-                    </div>
-                    <div className='w-20 h-0.5 mt-3 rounded-md bg-gray dark:bg-white' />
-                    <div className='w-2 h-20 rounded-md bg-triadic2 ml-auto' />
-                </div>
-                <div className='w-2 h-20 rounded-md' />
-                <div className='flex'>
-                    <div className='px-2 pb-2'>
-                        <p className='font-bold'>MDI</p>
-                        <p className='text-sm font-semibold'>Aug-Dec 2023</p>
-                        <p className='text-sm'>Controls Engineering Intern</p>
-                    </div>
-                    <div className='w-20 h-0.5 mt-3 rounded-md bg-gray dark:bg-white' />
-                    <div className='w-2 h-20 rounded-md bg-triadic2 ml-auto' />
-                </div>
-            </div>
-            <div className='px-1'>
-                <div className='flex'>
-                    <div className='w-2 h-144 rounded-md bg-gray dark:bg-white' />
-                    <div className='flex flex-col'>
-                        <div className='flex'>
-                            <div className='w-20 h-0.5 mt-3 rounded-md bg-gray dark:bg-white' />
-                        </div>
-                        <div className='px-2 mt-12 flex'>
-                            <div className='w-2 h-128 rounded-md bg-analagous1 dark:bg-white' />
-                            <div>
-                                <div className='w-20 h-0.5 mt-60 rounded-md bg-gray dark:bg-white' />
-                                <div className='px-2 pb-2 ml-20'>
-                                    <p className='font-bold'>CS Tutor</p>
-                                    <p className='text-sm font-semibold'>2021-2024</p>
-                                    <p className='text-sm'>Java and C</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className='px-2 pb-2'>
-                        <p className='font-bold'>York College of Pennsylvania</p>
-                        <p className='text-sm font-semibold'>2020-2024</p>
-                        <p className='text-sm'>B.S. Computer Engineering</p>
-                        <p className='text-sm'>Minor in Mathematics</p>
-                    </div>
-                </div>
-            </div>
+            <div className='flex-grow h-0.5' />
+            <div className='w-2 h-auto rounded-t-md bg-gray dark:bg-white' />
+            <div className='flex-grow h-0.5 mt-6 rounded-r-md bg-gray dark:bg-white' />
         </div>
-    </>
+        <div className='col-span-3 pt-3'>
+            <p className='font-bold'>York College of Pennsylvania</p>
+            <p className='text-sm font-semibold'>2020-2024</p>
+            <p className='text-sm'>B.S. Computer Engineering</p>
+            <p className='text-sm'>Minor in Mathematics</p>
+        </div>
+
+        {/* Row 2 */}
+        <div className='col-span-3'/>
+        <div className='flex justify-center'>
+            <div className='flex-grow h-0.5 mt-6' />
+            <div className='w-2 h-20' />
+            <div className='w-2 h-full mx-2 bg-gray dark:bg-white' />
+            <div className='w-2 h-full rounded-t-md bg-analagous1' />
+            <div className='flex-grow h-0.5 mt-6 rounded-r-md bg-gray dark:bg-white' />
+        </div>
+        <div className='col-span-3 pt-3'>
+            <p className='font-bold'>CS Tutor</p>
+            <p className='text-sm font-semibold'>2021-2024</p>
+            <p className='text-sm'>Java and C</p>
+        </div>
+
+        {/* Row 3 */}
+        <div className='col-span-3 pt-3 ml-auto'>
+            <p className='font-bold'>Becton Dickinson</p>
+            <p className='text-sm font-semibold'>May-Aug 2022</p>
+            <p className='text-sm'>Software Engineering Intern</p>
+        </div>
+        <div className='flex justify-center'>
+            <div className='flex-grow h-0.5 mt-6 rounded-r-md bg-gray dark:bg-white' />
+            <div className='w-2 h-20 rounded-md bg-triadic2' />
+            <div className='w-2 h-full mx-2 bg-gray dark:bg-white' />
+            <div className='w-2 h-full bg-analagous1' />
+            <div className='flex-grow h-0.5 mt-6' />
+        </div>
+        <div className='col-span-3'/>
+
+        {/* Row 4 */}
+        <div className='col-span-3'/>
+        <div className='flex justify-center'>
+            <div className='flex-grow h-0.5 mt-6' />
+            <div className='w-2 h-20' />
+            <div className='w-2 h-20 mx-2 bg-gray dark:bg-white' />
+            <div className='w-2 h-20 bg-analagous1' />
+            <div className='flex-grow h-0.5 mt-6' />
+        </div>
+        <div className='col-span-3' />
+
+        {/* Row 5 */}
+        <div className='col-span-3 pt-3 ml-auto'>
+            <p className='font-bold'>MDI</p>
+            <p className='text-sm font-semibold'>Jan-May 2023</p>
+            <p className='text-sm'>Controls Engineering Intern</p>
+        </div>
+        <div className='flex justify-center'>
+            <div className='flex-grow h-0.5 mt-6 rounded-r-md bg-gray dark:bg-white' />
+            <div className='w-2 h-20 rounded-md bg-triadic2' />
+            <div className='w-2 h-full mx-2 bg-gray dark:bg-white' />
+            <div className='w-2 h-full bg-analagous1' />
+            <div className='flex-grow h-0.5 mt-6' />
+        </div>
+        <div className='col-span-3'/>
+
+        {/* Row 6 */}
+        <div className='col-span-3'/>
+        <div className='flex justify-center'>
+            <div className='flex-grow h-0.5 mt-6' />
+            <div className='w-2 h-20' />
+            <div className='w-2 h-20 mx-2 bg-gray dark:bg-white' />
+            <div className='w-2 h-20 bg-analagous1' />
+            <div className='flex-grow h-0.5 mt-6' />
+        </div>
+        <div className='col-span-3'/>
+
+        {/* Row 7 */}
+        <div className='col-span-3 pt-3 ml-auto'>
+            <p className='font-bold'>MDI</p>
+            <p className='text-sm font-semibold'>Aug-Dec 2023</p>
+            <p className='text-sm'>Controls Engineering Intern</p>
+        </div>
+        <div className='flex justify-center'>
+            <div className='flex-grow h-0.5 mt-6 rounded-r-md bg-gray dark:bg-white' />
+            <div className='w-2 h-20 rounded-md bg-triadic2' />
+            <div className='w-2 h-full mx-2 bg-gray dark:bg-white' />
+            <div className='w-2 h-full bg-analagous1' />
+            <div className='flex-grow h-0.5 mt-6' />
+        </div>
+        <div className='col-span-3'/>
+
+        {/* Row 8 */}
+        <div className='col-span-3 pt-3'/>
+        <div className='flex justify-center'>
+            <div className='flex-grow h-0.5 mt-6' />
+            <div className='w-2 h-20' />
+            <div className='w-2 h-20 mx-2 rounded-b-md bg-gray dark:bg-white' />
+            <div className='w-2 h-20 rounded-b-md bg-analagous1' />
+            <div className='flex-grow h-0.5 mt-6' />
+        </div>
+        <div className='col-span-3'/>
+
+    </div>
   )
 }
 
