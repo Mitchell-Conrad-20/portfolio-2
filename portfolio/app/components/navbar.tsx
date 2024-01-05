39d5dcfd10371651da8fbf70db526f63b45c92af
Mitchell Conrad <Mitchell.conrad@gmail.com>
2024-01-05 18:07:37 -0500

    Fix nav bug

--- portfolio/app/components/navbar.tsx
+++ portfolio/app/components/navbar.tsx
@@ -3,8 +3,8 @@ import {BsFillMoonStarsFill} from 'react-icons/bs';
 
 const Navbar = ( props: { darkModeToggle:()=>void } ) => {
   return (
-    <nav className='py-10 flex justify-between'>
-        <h1 className='font-bold text-xl pt-2'>Mitchell Conrad</h1>
+    <nav className='py-10 flex justify-between align-middle'>
+        <h1 className='font-bold text-xl'>Mitchell Conrad</h1>
         <ul className='flex items-center'>
             <li><BsFillMoonStarsFill onClick={ () => props.darkModeToggle() } className='cursor-pointer text-2xl' /></li>
             <li><a href="#" className='text-white bg-gradient-to-br from-analagous1 to-primary px-4 py-2 rounded-md ml-4'>Resume</a></li>
