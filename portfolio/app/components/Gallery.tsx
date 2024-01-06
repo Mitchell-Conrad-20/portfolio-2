2446a91c35d8199c2c4aa21d68d52b53797372e4
Mitchell Conrad <Mitchell.conrad@gmail.com>
2024-01-05 21:24:39 -0500

    Update Gallery.tsx

--- portfolio/app/components/Gallery.tsx
+++ portfolio/app/components/Gallery.tsx
@@ -11,31 +11,25 @@ const Gallery = () => {
   return (
     <>
       <h3 className='text-xl text-center'>Projects</h3>
-      {/* Use two seperate rows to force a maximum of 3 cards per row. Not ideal since it is not dynamic. Can make it dynamic if needed */}
-      {/* First Row */} 
       <div className='flex flex-row flex-wrap justify-center py-10 gap-10 lg:px-24 xl:px-44'>
-          <Card src={rifle} alt='Rifle for VR Games' title='Immersive Recoil for VR'>
-            <p>Generates realistic recoil when firing weapons in VR. Simulates a variety of firearms using a rapid-firing pneumatic piston.</p>
-          </Card>
-          <Card src={robot} alt='Robot' title='Optical Wireless Comms.'>
-            <p>Developed a computer vision aided OWC system for autonomous mobile robots.</p>
-          </Card>
-          <Card src={drone} alt='Drone' title='Custom Drone'>
+        <Card src={rifle} alt='Rifle for VR Games' title='Immersive Recoil for VR'>
+          <p>Generates realistic recoil when firing weapons in VR. Simulates a variety of firearms using a rapid-firing pneumatic piston.</p>
+        </Card>
+        <Card src={robot} alt='Robot' title='Optical Wireless Comms.'>
+          <p>Developed a computer vision aided OWC system for autonomous mobile robots.</p>
+        </Card>
+        <Card src={drone} alt='Drone' title='Custom Drone'>
           <p>Created a drone using an Arduino based flight controller.</p>
-          </Card>
-        </div>
-
-        {/* Second Row */}
-        <div className='flex flex-row flex-wrap justify-center py-10 gap-10 lg:px-24 xl:px-44'>
-          <Card src={synth} alt='MIDI Synthesizer' title='Stepper Motor MIDI Synth'>
+        </Card>
+        <Card src={synth} alt='MIDI Synthesizer' title='Stepper Motor MIDI Synth'>
           <p>Processes live MIDI signals and creates music using the vibrations of stepper motors.</p>
-          </Card>
-          <Card src={paperTrader} alt='Screenshot of Website' title='PaperTrader'>
+        </Card>
+        <Card src={paperTrader} alt='Screenshot of Website' title='PaperTrader'>
           <p>PaperTrader gives users access to real stock market data and charts in order to make informed buy and sell decisions with play money.</p>
-          </Card>  
-          <Card src={heicTool} alt='Screenshot of HEIC Conversion Tool Software' title='HEIC Conversion Tool'>
+        </Card>  
+        <Card src={heicTool} alt='Screenshot of HEIC Conversion Tool Software' title='HEIC Conversion Tool'>
           <p>Allows users to convert entire folders of HEIC (iPhone) photos to JPG or PNG formats.</p>
-          </Card>            
+        </Card>            
       </div>
     </>
   )
