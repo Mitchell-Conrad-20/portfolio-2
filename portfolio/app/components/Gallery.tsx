aabe908e5c90e2ea2a638c0c8b253b15f101ea2d
Mitchell Conrad <Mitchell.conrad@gmail.com>
2024-01-05 21:17:55 -0500

    updated gallery

--- portfolio/app/components/Gallery.tsx
+++ portfolio/app/components/Gallery.tsx
@@ -10,27 +10,33 @@ import heicTool from '../../public/images/HEIC-Conversion-Tool-Graphic.png'
 const Gallery = () => {
   return (
     <>
-        <h3 className='text-xl text-center pt-10'>Gallery</h3>
-            <div className='flex flex-row flex-wrap justify-center'>
-                <Card src={rifle} alt='Rifle for VR Games' title='Immersive Recoil for VR'>
-                <p>Simulates realistic recoil when firing weapons in VR games. The recoil is generated using a pneumatic piston, which can fire rapidly at various intensities to simulate a variety of firearms.</p>
-                </Card>
-                <Card src={robot} alt='Robot' title='Optical Wireless Communications'>
-                <p>Developed a computer vision aided OWC system for autonomous mobile robots.</p>
-                </Card>
-                <Card src={drone} alt='Drone' title='Drone'>
-                <p>Created a drone using an Arduino based flight controller.</p>
-                </Card>
-                <Card src={synth} alt='MIDI Synthesizer' title='Stepper Motor MIDI Synth'>
-                <p>Processes live MIDI signals and creates music using the vibrations of stepper motors.</p>
-                </Card>
-                <Card src={paperTrader} alt='Screenshot of Website' title='PaperTrader'>
-                <p>PaperTrader gives users access to real stock market data and charts in order to make informed buy and sell decisions with play money.</p>
-                </Card>  
-                <Card src={heicTool} alt='Screenshot of HEIC Conversion Tool Software' title='HEIC Conversion Tool'>
-                <p>Allows users to convert entire folders of HEIC (iPhone) photos to JPG or PNG formats.</p>
-                </Card>            
-            </div>
+      <h3 className='text-xl text-center'>Projects</h3>
+      {/* Use two seperate rows to force a maximum of 3 cards per row. Not ideal since it is not dynamic. Can make it dynamic if needed */}
+      {/* First Row */} 
+      <div className='flex flex-row flex-wrap justify-center py-10 gap-10 lg:px-24 xl:px-44'>
+          <Card src={rifle} alt='Rifle for VR Games' title='Immersive Recoil for VR'>
+            <p>Generates realistic recoil when firing weapons in VR. Simulates a variety of firearms using a rapid-firing pneumatic piston.</p>
+          </Card>
+          <Card src={robot} alt='Robot' title='Optical Wireless Comms.'>
+            <p>Developed a computer vision aided OWC system for autonomous mobile robots.</p>
+          </Card>
+          <Card src={drone} alt='Drone' title='Custom Drone'>
+          <p>Created a drone using an Arduino based flight controller.</p>
+          </Card>
+        </div>
+
+        {/* Second Row */}
+        <div className='flex flex-row flex-wrap justify-center py-10 gap-10 lg:px-24 xl:px-44'>
+          <Card src={synth} alt='MIDI Synthesizer' title='Stepper Motor MIDI Synth'>
+          <p>Processes live MIDI signals and creates music using the vibrations of stepper motors.</p>
+          </Card>
+          <Card src={paperTrader} alt='Screenshot of Website' title='PaperTrader'>
+          <p>PaperTrader gives users access to real stock market data and charts in order to make informed buy and sell decisions with play money.</p>
+          </Card>  
+          <Card src={heicTool} alt='Screenshot of HEIC Conversion Tool Software' title='HEIC Conversion Tool'>
+          <p>Allows users to convert entire folders of HEIC (iPhone) photos to JPG or PNG formats.</p>
+          </Card>            
+      </div>
     </>
   )
 }
