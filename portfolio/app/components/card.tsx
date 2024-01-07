c621ee03ff33e862fc7bccf09afc7ad65d5c3e21
Mitchell Conrad <Mitchell.conrad@gmail.com>
2024-01-06 20:43:31 -0500

    Card Hover Animation

--- portfolio/app/components/card.tsx
+++ portfolio/app/components/card.tsx
@@ -3,7 +3,7 @@ import { StaticImageData } from "next/image"
 
 const Card = (props: { src: StaticImageData, alt: string, title: string, children: React.ReactNode}) => {
   return (
-    <div className="shadow-2xl rounded-lg w-72 md:h-80 dark:bg-[rgba(255,255,255,0.05)] cursor-pointer hover:scale-105">
+    <div className="shadow-2xl rounded-lg w-72 md:h-80 dark:bg-[rgba(255,255,255,0.05)] cursor-pointer transition ease-in-out hover:scale-105">
         <Image src={props.src} alt={props.alt} className="rounded-t-lg h-36 object-cover"/>
         <h3 className="font-bold pt-2 px-5">{ props.title }</h3>
         <div className="px-5 pb-5 pt-0">
